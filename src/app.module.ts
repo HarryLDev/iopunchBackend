912e89f7b82e10fbd3425993163003ff7c4ff8d6
Harry Liu <harry.s.liu@gmail.com>
2024-01-25 17:58:56 -0500

    CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Timer } from './timer/entities/timer.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TimerModule } from './timer/timer.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TimerModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [Timer],
+      synchronize: true,
+    }),
+    TimerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
