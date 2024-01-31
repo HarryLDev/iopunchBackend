b3ae2889ae76cabccbd85582b7cc02ef83010e30
Harry Liu <harry.s.liu@gmail.com>
2024-01-31 16:37:53 -0500

    WORKING

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cors = require("cors")
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, {cors: true});
+  app.enableCors({
+    origin: 'http://localhost:4200/'
+  });
   await app.listen(3000);
 }
 bootstrap();
